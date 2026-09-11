e83762fb58a731cfe3ab7bc7dae8f4b652f98ef8
Eric Oligney <EOengineer@gmail.com>
2026-09-11 15:27:27 -0400

    adds admin users show view

--- frontend/app/lib/admin/users.api.ts
+++ frontend/app/lib/admin/users.api.ts
@@ -14,3 +14,17 @@ export async function fetchUsers(): Promise<User[]> {
 
   return (await response.json()) as User[];
 }
+
+export async function fetchUser(id: string): Promise<User> {
+  const response = await fetch(apiUrl(`/admin/users/${id}`), {
+    method: 'GET',
+    headers: defaultHeaders(),
+    credentials: 'include',
+  });
+
+  if (!response.ok) {
+    throw await parseApiError(response);
+  }
+
+  return (await response.json()) as User;
+}
